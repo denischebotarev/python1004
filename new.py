20881bd4f937c036e0f7b2dff36bb10925c3fbce
denischebotarev <denys.chebotarev@gmail.com>
2017-05-17 23:08:26 +0300

    try

--- new.py
+++ new.py
@@ -1,10 +1,7 @@
-import requests
+x = 10
 
-url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
-key = 'trnsl.1.1.20170509T173115Z.7389ba585fa6cfbd.673ddf47a800eab1bf15efb75ffdc3db68bcba5f'
-text = 'привет'
-lang = 'ru-en'
-r = requests.post(url, data={'key': key, 'text': text, 'lang': lang})
+def new(y=x):
+    y += x
+    return y
 
-print(r.text)
-print(r)
+print(new())
